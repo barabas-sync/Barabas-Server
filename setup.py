3cc5eb3349ca398eb51030cee8780f94f0a32288
Nathan Samson <nathansamson@gmail.com>
2011-08-12 13:30:24 +0200

    Making deploying easier

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Barabas',
+      version='0.1',
+      description='Barabas Server Program',
+      author='Nathan Samson',
+      author_email='nathan.samson@student.ua.ac.be',
+      url='http://github.com/barabas-sync/Barabas-Server/',
+      packages=['barabas', 'barabas.database', 'barabas.identity',
+                'barabas.network', 'barabas.network.channels',
+                'barabas.network.terminals', 'barabas.objects'],
+      scripts=['barabasd'],
+      data_files=[('/etc', ['barabas-server.cfg.example'])]
+     )
